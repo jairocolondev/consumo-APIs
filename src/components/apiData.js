f142f3fb917517526e6e393405edaabd3ac8d008
Jairo  Colón <jairoc@titamedia.com>
2023-07-18 10:23:48 -0500

    Render as you Fetch

--- src/components/apiData.js
+++ src/components/apiData.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import useFetch from "@/hooks/useFetch";
 import styles from "@/styles/Home.module.css";
+import fetchData from "@/hooks/fetchData";
+
+const apiData = fetchData("https://jsonplaceholder.typicode.com/users");
 
 function ApiData() {
+  const data = apiData.read();
   return (
     <div className={styles.containerDataFetch}>
       <h1>Data con fetch para TopHeader MEM</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className="card">
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </Suspense>
     </div>
   );
 }
--- src/hooks/fetchData.js
+++ src/hooks/fetchData.js
@@ -1,3 +1,38 @@
-function fetchData() {}
+function getSuspender(promise) {
+  let status = "pending";
+  let response;
+
+  const suspender = promise
+    .then((res) => {
+      status = "success";
+      response = res;
+    })
+    .catch((err) => {
+      status = "error";
+      response = err;
+    });
+
+  const read = () => {
+    switch (status) {
+      case "pending":
+        throw suspender;
+      case "error":
+        throw response;
+      default:
+        return response;
+    }
+  };
+  return {
+    read,
+  };
+}
+
+function fetchData(url) {
+  const promise = fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+
+  return getSuspender(promise);
+}
 
 export default fetchData;
